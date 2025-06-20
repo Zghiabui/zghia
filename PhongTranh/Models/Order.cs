104519e2d1205168f15178f9998e656218d4960a
Zghiabui <buitrongnghiaaaaa@gmail.com>
2025-06-20 11:37:34 +0700

    alo

---
+++ PhongTranh/Models/Order.cs
@@ -0,0 +1,10 @@
+namespace PhongTranh.Models
+{
+    public class Category
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
