8b00ea7d173453fc645384e4e1a5fb5203ec9641
Zghiabui <buitrongnghiaaaaa@gmail.com>
2025-06-20 08:55:45 +0700

    first commit

---
+++ PhongTranh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhongTranh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
